c9297f4bc87ac91a198534ef3520bd2e5be0566d
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-10-23 20:21:44 -0300

    Change export to pure HTML

--- doc/src/SUMMARY.md
+++
@@ -1,9 +0,0 @@
-# Sumário
-
--   [Teste](./teste.md)
--   [Agradecimentos](./00-agradecimentos.md)
--   [Introdução](./01-introducao.md)
-    -   [O que é Lisp?](./01-01-o-que-e-lisp.md)
-    -   [Majestic Lisp](./01-02-majestic-lisp.md)
-    -   [Sobre Programação Instruída](./01-03-sobre-programacao-instruida.md)
-    -   [Como ler este documento](./01-04-como-ler-este-documento.md)
--- publish.el
+++ publish.el
@@ -1,9 +1,31 @@
 (require 'ox-publish)
 (require 'ox-gfm)
+(require 'ox-html)
+
+(setq *publishing-directory* (concat default-directory "doc/html/"))
+
+(defun publish-subdir (dir)
+  (concat *publishing-directory* dir))
 
 (setq org-publish-project-alist
-      `(("majestic-book"
+      `(
+        ("static-images"
+         :base-directory ,(concat default-directory "img/")
+         :base-extension "png\\|jpg\\|jpeg"
+         :publishing-directory ,(publish-subdir "img/")
+         :recursive t
+         :publishing-function org-publish-attachment)
+        ("static-files"
+         :base-directory ,(concat default-directory "static/")
+         :base-extension "js\\|css"
+         :publishing-directory ,*publishing-directory*
+         :recursive t
+         :publishing-function org-publish-attachment)        
+        ("html"
          :base-directory ,default-directory
-         :publishing-directory ,(concat default-directory "doc/src/")
+         :publishing-directory ,*publishing-directory*
          :base-extension "org"
-         :publishing-function org-gfm-publish-to-gfm)))
+         :exclude "setupfile.org"
+         :publishing-function org-html-publish-to-html)
+        ("majestic"
+         :components ("static-images" "static-files" "html"))))
