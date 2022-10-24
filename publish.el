c2286a5b1138b7be62a78bb9ca126fb5865425c6
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-10-23 21:20:56 -0300

    Add configuration for tangling project files

--- publish.el
+++ publish.el
@@ -27,5 +27,11 @@
          :base-extension "org"
          :exclude "setupfile.org"
          :publishing-function org-html-publish-to-html)
+        ("tangle"
+         :base-directory ,default-directory
+         :publishing-directory ,default-directory
+         :recursive t
+         :exclude "static/\\|setupfile.org"
+         :publishing-function org-babel-tangle-publish)
         ("majestic"
          :components ("static-images" "static-files" "html"))))
